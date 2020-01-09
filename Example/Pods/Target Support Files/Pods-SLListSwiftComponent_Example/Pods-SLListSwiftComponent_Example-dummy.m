19f9e1cade3dc661c966d82d4418d0320ab1817b
li shuai <lishuai19@yeah.net>
2020-01-09 11:34:14 +0800

    初始化

---
+++ Example/Pods/Target Support Files/Pods-SLListSwiftComponent_Example/Pods-SLListSwiftComponent_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SLListSwiftComponent_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SLListSwiftComponent_Example
+@end
---
+++ Example/SLListSwiftComponent/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SLListSwiftComponent
+//
+//  Created by lishuailibertine on 01/09/2020.
+//  Copyright (c) 2020 lishuailibertine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
